f42c33304fa9c54967827fd27c3f1e22c7d7d8c1
istiyakaminsanto <istiyakaminsanto@gmail.com>
2022-02-15 12:26:43 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./public/componets/**/*.{html,js}",
+    "./public/template/**/*.{html,js}",
+    // "./src/**/*.{html,js}",
+    // "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
